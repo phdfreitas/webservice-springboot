71e3c4afaa5a2bc8d7ad0649aa66b5935fec12eb
phdfreitas <pedro.hnd.freitas@gmail.com>
2022-06-28 07:57:33 -0300

    Iniciando projeto e testando endpoint de usuário

---
+++ src/main/java/com/springboot/webservice/WebserviceSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebserviceSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebserviceSpringbootApplication.class, args);
+    }
+
+}
